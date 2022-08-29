acad2e4ae97b25d6cf8ebca4169c93d0ee224daf
Kai <b790718@gmail.com>
2022-08-29 17:37:22 +0800

    import animation & transition effect to poll

--- src/lib/components/PollList.svelte
+++ src/lib/components/PollList.svelte
@@ -1,26 +1,14 @@
 <script>
+  import { fade, scale } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
+  import pollsStore from '../stores/PollStore';
   import PollDetails from './PollDetails.svelte';
-  export let polls = [];
-
-  const voteHandler = (e) => {
-    const { option, id } = e.detail;
-
-    let copiedPolls = [...polls];
-    let upVotedPoll = copiedPolls.find((poll) => poll.id === id);
-
-    if (option === 'a') {
-      upVotedPoll.votesA++;
-    }
-    if (option === 'b') {
-      upVotedPoll.votesB++;
-    }
-
-    polls = copiedPolls;
-  };
 </script>
 
 <div class="grid grid-cols-2 gap-5">
-  {#each polls as poll (poll.id)}
-    <PollDetails {poll} on:vote={voteHandler} />
+  {#each $pollsStore as poll (poll.id)}
+    <div in:fade out:scale|local animate:flip={{ duration: 500 }}>
+      <PollDetails {poll} />
+    </div>
   {/each}
 </div>
